faaebfa242c86341ffc5b0578c146bf1f7c41b01
liyi10 <68773379+liyi10@users.noreply.github.com>
2020-07-28 14:58:50 +0800

    猜年龄游戏

--- 14.py
+++ 14.py
@@ -1,1 +1,23 @@
-print(*(i for i in range(2000, 3201) if i%7 == 0 and i%5 != 0), sep=",")
+import random
+times=0
+computer = random.randint(1, 100)
+while times<3:
+    admin = int(input('猜年龄:'))
+    if admin==computer:
+        print('恭喜您猜对了')
+        break
+    elif admin>computer:
+        print('您猜测的年龄大了')
+    else:
+        print('您猜测的年龄小了')
+    times = times + 1
+    if times==3:
+        print('三次机会用完了，您是否继续?\n[N or n:否,Y or y:是]')
+        again = input()
+        if again == 'N' or again == 'n':
+            times=4
+            print('游戏结束')
+        elif again == 'Y' or again == 'y':
+            times=0
+        else:
+            print('不要乱输入')
